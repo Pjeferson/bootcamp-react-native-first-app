a3dfd73ee3a04b645109d543d2b90db21656e3d8
Pjeferson <paulojeferson34@gmail.com>
2020-01-06 23:59:46 -0300

    adding navigation

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {} from 'react-native';
+
 import './src/config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import Routes from './src/routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <Routes />;
 }
-console.tron.log('hello');
