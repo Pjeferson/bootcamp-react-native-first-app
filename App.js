857a08a08ac018475eaee73da2a3d660eba33e3e
Pjeferson <paulojeferson34@gmail.com>
2020-01-06 21:52:37 -0300

    configuring eslint and prettier

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
@@ -11,8 +11,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
